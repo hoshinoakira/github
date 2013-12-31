d25221c12444267c2f6a463d8f00d8134b2d4e79
Ta Minh Ha <ta.minh.ha@aainc.co.jp>
2013-12-31 11:14:33 +0900

    add content to config.php file

--- config.php
+++ config.php
@@ -1,3 +1,5 @@
 <?php
 echo "this is config file";
+echo "git rev-list -n 1 HEAD -- <file_path>"; //lay head cua file da bi xoa
+echo "git checkout <deleting_commit>^ -- <file_path>"; //recover deleted file
 ?>
